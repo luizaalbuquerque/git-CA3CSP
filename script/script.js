713569210ab64132105c1c3cda955b96cdf8c613
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-04-19 14:53:00 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
-function validate(){
-    var passwordValidator = document.getElementById("passwordValidator").nodeValue;
-var regx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+// password validator 
 
+function validate(){
+    // access the value inside the text box (id passwordValidator)
+    var passwordValidator = document.getElementById("passwordValidator").nodeValue;
+// creating a regex object (literal)
+    var regx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    // method used for matching the regex, will return valid or invalid
 if (regx.test(passwordValidator)){
     alert("Valid password")
     document.getElementById('Invalid').stylevisibility ="hidden";
