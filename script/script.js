8a34b33bad944b0bf48afc2f3628a713316b76a3
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-04-29 16:03:33 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,24 +2,22 @@
 
 function validate() {
     // access the value inside the text box (id passwordValidator)
-    var passwordValidator = document.getElementById("passwordValidator");
+    var passwordValidator = document.getElementById("PasswordValidator").Pvalidator;
     // creating a regex object (literal)
-    var regx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    var regx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    alert(passwordValidator)
 
     // method used for matching the regex, will return valid or invalid
     if (regx.test(passwordValidator)) {
         alert('Valid password')
-        document.getElementById('Invalid').stylevisibility = "hidden";
-        document.getElementById('Valid').style.visibility = "visible";
+ 
 
     }
     else {
         alert('Invalid password');
-        document.getElementById('Valid').style.visibility = "hidden";
-        document.getElementById('Invalid').style.visibility = "visible";
+       
     }
-
-
+}
 
 
     function myFunction() {
