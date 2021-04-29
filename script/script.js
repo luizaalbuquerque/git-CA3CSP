7d28ae9cf53bae6f4111355605fda187c4e3d43c
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-04-29 12:35:37 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 
 function validate() {
     // access the value inside the text box (id passwordValidator)
-    var passwordValidator = document.getElementById("passwordValidator").nodeValue;
+    var passwordValidator = document.getElementById("passwordValidator");
     // creating a regex object (literal)
     var regx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
@@ -18,5 +18,12 @@ function validate() {
         document.getElementById('Valid').style.visibility = "hidden";
         document.getElementById('Invalid').style.visibility = "visible";
     }
+
+
+
+
+    function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
 }
 
