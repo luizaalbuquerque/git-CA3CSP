ddeb6ee6ac1f9cf390f2278ab36f9fb190e50bf9
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-05-02 18:59:20 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -9,21 +9,96 @@ function validate() {
 
     // method used for matching the regex, will return valid or invalid
     if (regx.test(passwordValidator)) {
-       var x = document.getElementById("valid");
-       x.style.display = "block";
-       var y = document.getElementById("inValid");
-       y.style.display = "none";
+       var password = document.getElementById("valid");
+       password.style.display = "block";
+       var other = document.getElementById("inValid");
+       other.style.display = "none";
  
 
     }
     else {
-        var x = document.getElementById("inValid");
-        x.style.display = "block";
-        var y = document.getElementById("valid");
-        y.style.display = "none";
+        var password = document.getElementById("inValid");
+        password.style.display = "block";
+        var other = document.getElementById("valid");
+        other.style.display = "none";
     }
 }
 
+function Starters(){
 
+    var els = document.getElementById('starters').getElementsByTagName("input");
+    var totaldiv = 0;
+    alert(els);
 
+    for (var i =0; i < els.length;i++)
+    {
+        multiplicacao = parseFloat(els[i].getAttribute("data-price") * parseFloat(els[i].value));
+        totaldiv = totaldiv + multiplicacao;
+        alert(totaldiv);
+    }}
+
+    function Main(){
+
+        var els = document.getElementById('main').getElementsByTagName("input");
+        var totaldiv = 0;
+    
+        for (var i =0; i < els.length;i++)
+        {
+            multiplicacao = parseFloat(els[i].getAttribute("data-price") * parseFloat(els[i].value));
+            totaldiv = totaldiv + multiplicacao;
+            alert(totaldiv);
+        }
+    
+    
+        
+    
+    }
+    function Desserts(){
+
+        var els = document.getElementById('desserts').getElementsByTagName("input");
+        var totaldiv = 0;
+    
+        for (var i =0; i < els.length;i++)
+        {
+            multiplicacao = parseFloat(els[i].getAttribute("data-price") * parseFloat(els[i].value));
+            totaldiv = totaldiv + multiplicacao;
+            alert(totaldiv);
+        }
+    
+    
+        
+    
+    }   
+    function Drinks(){
+
+        var els = document.getElementById('drinks').getElementsByTagName("input");
+        var totaldiv = 0;
+    
+        for (var i =0; i < els.length;i++)
+        {
+            multiplicacao = parseFloat(els[i].getAttribute("data-price") * parseFloat(els[i].value));
+            totaldiv = totaldiv + multiplicacao;
+            alert(totaldiv);
+        }
+    
+    
+        
+    
+    }
+    function Total(){
+
+        var els = document.getElementById('Total').getElementsByTagName("input");
+        var totaldiv = 0;
+    
+        for (var i =0; i < els.length;i++)
+        {
+            multiplicacao = parseFloat(els[i].getAttribute("data-price") * parseFloat(els[i].value));
+            totaldiv = totaldiv + multiplicacao;
+            alert(totaldiv);
+        }
+    
+    
+        
+    
+    }
 
