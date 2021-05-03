a7f0736f48a2b03b708c4815cf8724dee600237a
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-05-03 20:07:16 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -5,17 +5,18 @@ function validate() {
     var passwordValidator = document.getElementById("PasswordValidator").value;
     // creating a regex object (literal)
     var regx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-   
+
 
     // method used for matching the regex, will return valid or invalid
     if (regx.test(passwordValidator)) {
-       var password = document.getElementById("valid");
-       password.style.display = "block";
-       var other = document.getElementById("inValid");
-       other.style.display = "none";
- 
+        var password = document.getElementById("valid");
+        password.style.display = "block";
+        var other = document.getElementById("inValid");
+        other.style.display = "none";
+
 
     }
+    
     else {
         var password = document.getElementById("inValid");
         password.style.display = "block";
@@ -23,20 +24,20 @@ function validate() {
         other.style.display = "none";
     }
 }
-//customer
+//customer generator
 
 window.onload = CallJson;
 
-        function CallJson(){
-            fetch('https://randomuser.me/api/?results=6')
-            .then((load) => load.json())
-            .then((data) => {
-                let datas = data.results;
-                let output = '<h2>Customers</h2>'
-            
-                console.log(data);
-                datas.forEach(function(costumer){
-                    output += `
+function CallJson() {
+    fetch('https://randomuser.me/api/?results=6')
+        .then((load) => load.json())
+        .then((data) => {
+            let datas = data.results;
+            let output = '<h2>Customers</h2>'
+
+            console.log(data);
+            datas.forEach(function (costumer) {
+                output += `
                     <div class="boxCustomer">
                         <ul>
 
@@ -62,62 +63,49 @@ window.onload = CallJson;
                         </ul>
                     </div>
                     `;
-                });
+            });
 
-                document.getElementById('CustumerGenerator').innerHTML = output;
+            document.getElementById('CustumerGenerator').innerHTML = output;
 
-            })
-        }
-
-
+        })
+}
 
 
+// totals generator
+function total(namediv) {
 
+    var veganItens = new Array("isso é evegano", "Mushroom Brushchetta with Balsamic and Thyme", "Creamy Brocoli pasta", "Vegan Lemon Cheesecake")
 
-
-// totals
-function total(namediv){
-
-    var veganItens = new Array("isso é evegano","Mushroom Brushchetta with Balsamic and Thyme","Creamy Brocoli pasta","Vegan Lemon Cheesecake")
-
-    var els = document.getElementById(namediv).getElementsByTagName("input");
+    var el = document.getElementById(namediv).getElementsByTagName("input");
     var totaldiv = 0;
     var pricevegan = 0;
     var pricenovegan = 0;
 
 
-    for (var i =0; i < els.length;i++)
-    {
-   
-var veganorno = ((veganItens.indexOf(els[i].getAttribute("name"))) > -1);
+    for (var i = 0; i < el.length; i++) {
 
-        if (veganorno === true) 
-        {   
-          pricevegan =  pricevegan + (parseFloat(els[i].getAttribute("data-price") * parseFloat(els[i].value)));
-        
-            pricenovegan =totaldiv;
+        var veganorno = ((veganItens.indexOf(el[i].getAttribute("name"))) > -1);
+
+        if (veganorno === true) {
+            pricevegan = pricevegan + (parseFloat(el[i].getAttribute("data-price") * parseFloat(el[i].value)));
+
+            pricenovegan = totaldiv;
+
+            var ItemsTotal = document.getElementById("veg" + namediv)
+            ItemsTotal.innerHTML = "€" + pricevegan;
 
 
-            
-    var ItemsTotal = document.getElementById("veg"+ namediv)
-    ItemsTotal.innerHTML = "€" + pricevegan;
-    // validate = 1;
+            var ItemsTotal = document.getElementById("non-veg" + namediv)
+            ItemsTotal.innerHTML = "€" + pricenovegan;
 
-    var ItemsTotal = document.getElementById("non-veg"+namediv)
-    ItemsTotal.innerHTML = "€" + pricenovegan;
-
-            
         }
-        
-        
-        multiply = parseFloat(els[i].getAttribute("data-price") * parseFloat(els[i].value));
-        totaldiv = totaldiv + multiply;
-     
-     
-    }
-    
 
-    var ItemsTotal = document.getElementById("ItemsTotal"+ namediv)
+        multiply = parseFloat(el[i].getAttribute("data-price") * parseFloat(el[i].value));
+        totaldiv = totaldiv + multiply;
+
+    }
+
+    var ItemsTotal = document.getElementById("ItemsTotal" + namediv)
     ItemsTotal.innerHTML = "€" + totaldiv;
 }
 
