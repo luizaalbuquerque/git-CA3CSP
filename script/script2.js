8eabf356deb8fb7ee54756a697221aeed201c344
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-05-04 21:33:39 +0100

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -1,4 +1,14 @@
-function change(){
-    document.getElementById("demo").innerHTML="img";
+function hideImage() {
+  $('.Restaurant').hide();
 
+}
+window.onload = hideImage;
+
+function showImage(id)
+{
+  $(id).show();
+  $(id).show().animate({"margin-top": '-50px'})
+  $(id).show().animate({"margin-top": '50px'})
+
+  $(id).show().animate({"margin-top": '0px'})
 }
