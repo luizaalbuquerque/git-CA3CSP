18fc95a743a64b4aeeec4f111bbf4cdd04f44a5e
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2021-05-11 11:17:36 +0100

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -1,14 +1,19 @@
+// menu JS 
+
+//function to hide images on the page using jQuery
 function hideImage() {
-  $('.Restaurant').hide();
+  $('.Restaurant').hide(); //gets the class from the img and hides it 
 
 }
-window.onload = hideImage;
+window.onload = hideImage; // load the JS (hide the imgage) straight after the page is loaded 
 
+//function to show images after pressing a button on the page using jQuery
 function showImage(id)
 {
-  $(id).show();
+  $(id).show(); //shows the id (img)
+  // animation of the image after pressing the button 
   $(id).show().animate({"margin-top": '-50px'})
   $(id).show().animate({"margin-top": '50px'})
-
+// puts the image back to it's original place
   $(id).show().animate({"margin-top": '0px'})
 }
